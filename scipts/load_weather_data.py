1c75e23d52e55349e92aa2efb2317077ad1fc10e
DanielFay22 <dpf2@rice.edu>
2020-02-10 17:17:04 -0600

    Adds comments to make Denizhan happy

--- scipts/load_weather_data.py
+++ scipts/load_weather_data.py
@@ -15,6 +15,8 @@ stations_output_path = None
 
 station_data = []
 
+# Retrieve the file containing station data from the url above,
+# and parse it into distinct fields
 with urllib.request.urlopen(station_path) as fn:
     for sb in fn.readlines():
         row = []
@@ -31,7 +33,7 @@ with urllib.request.urlopen(station_path) as fn:
         row.append(s[80:].strip())
         station_data.append(row)
 
-
+# Construct a dataframe containing the data for all stations
 stations = pd.DataFrame(
     station_data,
     columns=['station_id', 'lat', 'lon', 'elev', 'state', 'name', 'gsn_flag', 'hcn/crn_flag', 'wmo_id']
@@ -42,6 +44,7 @@ tx_stations = stations.loc[stations['state'] == 'TX']
 
 column_headers = ['station_id', 'date', 'reading_type', 'value', 'm_flag', 'q_flag', 's_flag', 'time']
 
+# Retrieve all weather data from start_year to end_year, inclusive
 start_year = 1950
 end_year = 2020
 
@@ -50,17 +53,23 @@ years = list(range(start_year, end_year + 1))
 frames = []
 tx_ids = tx_stations.station_id.values
 
+# For each year, download the file for that year to a local file,
+# decompress it using gzip, and create a dataframe with all the Texas data.
+# Deletes the downloaded file after completion.
 for year in years:
     fname = f"{year}.csv.gz"
 
+    # Download the compressed data
     with open(fname, "wb") as fn:
         r = requests.get(base_url + fname)
         fn.write(r.content)
 
+    # Read the compressed data into a file-like object
     f = gzip.open(fname, 'rb')
     data = StringIO(f.read().decode('utf-8'))
     f.close()
 
+    # Create a dataframe containing only data from Texas stations
     df = pd.read_csv(data, header=None, names=column_headers)
     df = df.loc[df.station_id.isin(tx_ids)]
     frames.append(df)
@@ -69,9 +78,10 @@ for year in years:
 
     print(f"Data loaded for {year}")
 
+# Join each years data together into a single dataframe
 all_df = pd.concat(frames)
 
-
+# If a local path is provided, the data and station dataframes are saved to a local file
 if data_output_path is not None:
     all_df.to_csv(data_output_path, index=False)
 if stations_output_path is not None:
