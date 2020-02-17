5aff21627c7ffdd2ab9d47bd3c41ccb6cc0d9ad2
DanielFay22 <dpf2@rice.edu>
2020-02-17 17:43:02 -0600

    Corrects stupid change

--- runtime/util/DataViz.py
+++ runtime/util/DataViz.py
@@ -4,7 +4,7 @@ import calendar
 import gmplot
 import matplotlib.dates as mdates
 
-from .WeatherVSPothole import WeatherVSPotholes
+from util.WeatherVSPothole import WeatherVSPotholes
 
 
 class DataViz():
