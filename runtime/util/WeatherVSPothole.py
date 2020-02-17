1e3e342e3576a0eb1d5ed5dc2490e2910a43ba52
Denizhan Yigitbas <denizhanyigitbas@gmail.com>
2020-02-16 18:29:18 -0600

    2019-2020 data pull

--- runtime/util/WeatherVSPothole.py
+++ runtime/util/WeatherVSPothole.py
@@ -1,6 +1,10 @@
 from collections import deque
 
 import pandas as pd
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 weather_data = "../../data/raw/all_tx_weather.csv"
 
@@ -8,14 +12,21 @@ class WeatherVSPotholes():
     def __init__(self):
         pass
     
-    def explore(self):
-        df = pd.read_csv(weather_data)
-        print(df)
-        # with open(weather_data, 'r') as f:
-        #     q = deque(f, 2)  # replace 2 with n (lines read at the end)
-        # print(q)
-        
+    def create_last_n_rows_df(self, n):
+        with open(weather_data, 'r') as f:
+            q = deque(f, n)  # replace 2 with n (lines read at the end)
+        column_names = ['station_id', 'date', 'reading_type', 'value', 'm_flag', 'q_flag', 's_flag', 'time']
+        df = pd.read_csv(StringIO(''.join(q)), header=None)
+        df.columns = column_names
+        return df
+    
+    def create_2019_2020_df(self):
+        df = self.create_last_n_rows_df(2000000)
+        one_year_row = df.loc[df["date"] == 20190208].index.values[0]
+        one_year_df = df.loc[one_year_row:]
+        return one_year_df
+    
         
 if __name__ == "__main__":
     comparer = WeatherVSPotholes()
-    comparer.explore()
+    comparer.create_2019_2020_df()
