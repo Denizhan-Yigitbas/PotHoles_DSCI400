f130046fdc808c4851ce1a2db1028a9058a51ed4
DanielFay22 <dpf2@rice.edu>
2020-02-17 18:26:15 -0600

    Minor changes to weatherVSpothole

--- runtime/util/WeatherVSPothole.py
+++ runtime/util/WeatherVSPothole.py
@@ -6,44 +6,46 @@ try:
 except ImportError:
     from io import StringIO
 
-weather_data = "../data/raw/all_tx_weather.csv"
+weather_data = "../../data/raw/all_tx_weather.csv"
 
 class WeatherVSPotholes():
     def __init__(self):
         pass
-    
-    """
-    Private method that extracts the last n rows of the all_tx_weather.csv file in order to access the
-     most recent recordings
-    """
-    def __create_last_n_rows_df(self, n):
+
+    @staticmethod
+    def __create_last_n_rows_df(n):
+        """
+        Private method that extracts the last n rows of the all_tx_weather.csv file in order to access the
+         most recent recordings
+        """
         with open(weather_data, 'r') as f:
             q = deque(f, n)  # replace 2 with n (lines read at the end)
         column_names = ['station_id', 'date', 'reading_type', 'value', 'm_flag', 'q_flag', 's_flag', 'time']
         df = pd.read_csv(StringIO(''.join(q)), header=None)
         df.columns = column_names
         return df
-    
-    """
-    Creates a DataFrame that contains the weather recording between Feb 2019 and Feb 2020
-    """
+
     def create_2019_2020_df(self):
+        """
+        Creates a DataFrame that contains the weather recording between Feb 2019 and Feb 2020
+        """
         df = self.__create_last_n_rows_df(2000000)
         one_year_row = df.loc[df["date"] == 20190208].index.values[0]
         one_year_df = df.loc[one_year_row:]
         return one_year_df
-    
-    """
-    Private method that extracts only the precipation recordings from the weather stations
-    """
-    def __extract_precip_data(self, weather_df):
+
+    @staticmethod
+    def __extract_precip_data(weather_df):
+        """
+        Private method that extracts only the precipation recordings from the weather stations
+        """
         precip_df = weather_df[weather_df["reading_type"] == "PRCP"]
         return precip_df
-    
-    """
-    Initial exploration method for a single station
-    """
+
     def single_station_explore(self, weather_df, station_id):
+        """
+        Initial exploration method for a single station
+        """
         p_df = self.__extract_precip_data(weather_df)
         single = p_df[p_df["station_id"] == station_id]
         single["date"] = pd.to_datetime(single["date"], format='%Y%m%d')
