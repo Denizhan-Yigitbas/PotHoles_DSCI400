6a02ff606f3fbf7df63febfd4c76a74727b4d92a
Denizhan Yigitbas <denizhanyigitbas@gmail.com>
2020-03-03 10:18:08 -0600

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PotHoles_DSCI400',
+    version='1.1',
+    packages=['', 'runtime', 'runtime.util', 'runtime.dataloader'],
+    url='',
+    license='',
+    author='Denizhan Yigitbas',
+    author_email='dy22@rice.edu',
+    description='Potholes'
+)
