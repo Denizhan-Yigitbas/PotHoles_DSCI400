d73b7cd8704cf2b4c7a4c8efbc72be5865ce46ca
Denizhan Yigitbas <denizhanyigitbas@gmail.com>
2020-03-04 01:14:53 -0600

    fml

--- runtime/util/WeatherVSPothole.py
+++ runtime/util/WeatherVSPothole.py
@@ -1,8 +1,9 @@
 from shapely.geometry import Point, Polygon
 from datetime import datetime
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
 import calendar
+from mpl_toolkits.axes_grid1 import host_subplot
+import mpl_toolkits.axisartist as AA
 
 from runtime.dataloader import WeatherData, PotholeData
 
@@ -67,19 +68,20 @@ class WeatherVSPotholes():
         counts_df.set_index("month-year", inplace=True)
     
         # visualize the data
-        fig, ax = plt.subplots()
+        ax = host_subplot(111, axes_class=AA.Axes)
+        plt.subplots_adjust(bottom=.2, left=0.11, right=0.87)
+
+        
         x = counts_df.index
         y = counts_df["SR CREATE DATE"]
-        p1, = ax.bar(x, y, width=0.7, label="Potholes")
-        # ax.set_title("Pothole formation around station " + station_id)
-        # ax.xaxis.set_major_formatter(mdates.DateFormatter("%b %Y"))
-        # ax.xaxis.set_minor_formatter(mdates.DateFormatter("%b %Y"))
-        # ax.xaxis.set_major_locator(mdates.MonthLocator(interval=6))
-        # ax.tick_params(axis='x', rotation=45)
-        # ax.set_xlabel("Date")
-        # ax.set_ylabel("Number of Pothole")
-        # ax.legend(loc=0)
-    
+
+        ax.bar(x, y,  label="Potholes")
+        ax.set_title("Pothole Formation Around Station " + station_id + " With Weather")
+        ax.axis["bottom"].major_ticklabels.set_rotation(55)
+        ax.axis["bottom"].major_ticklabels.set_pad(19)
+        ax.axis["bottom"].label.set_pad(23)
+        ax.axis["top"].toggle(all=False)
+
         ax_prcp = ax.twinx()
         ax_temp = ax.twinx()
 
@@ -90,33 +92,37 @@ class WeatherVSPotholes():
                                             offset=(offset, 0))
 
         ax_temp.axis["right"].toggle(all=True)
+        ax_prcp.axis["right"].toggle(all=True)
 
         ax.set_xlabel("Time")
         ax.set_ylabel("Potholes")
-        ax_prcp.set_ylabel("Precip")
+        ax_prcp.set_ylabel("Precipitation")
         ax_temp.set_ylabel("Temperature")
-        
-        
+        #
+        #
         station_precip = WeatherData().avg_station_precipitation_per_month(2015, 2019, station_id)
         x = station_precip.index
         y = station_precip
-        p2, = ax_prcp.plot(x, y, color='r', linewidth=3, label="Precipitation")
-        # ax2.set_ylabel("Precipitation (tenths of mm)")
-        
+        p1, = ax_prcp.plot(list(range(len(x))), y, color='r', linewidth=3, label="Precipitation")
+        # # ax2.set_ylabel("Precipitation (tenths of mm)")
+        #
         station_temp = WeatherData().avg_station_temp_per_month(2015, 2019, station_id)
         x = station_temp.index
         y = station_temp
-        p3, = ax_temp.plot(x, y, color='g', linewidth=3, label="Temperature")
+        p2, = ax_temp.plot(list(range(len(x))), y, color='g', linewidth=3, label="Temperature")
         # ax2.legend(loc = 1)
         # ax2.set_ylabel("Temperature (tenths of degree Celcius)")
 
-        ax.axis["left"].label.set_color(p1.get_color())
-        ax_prcp.axis["right"].label.set_color(p2.get_color())
-        ax_temp.axis["right"].label.set_color(p3.get_color())
-        
-        plt.subplots_adjust(bottom=.2, left=0.11, right=0.87)
+        ax_prcp.axis["right"].label.set_color(p1.get_color())
+        ax_temp.axis["right"].label.set_color(p2.get_color())
+        # ax_temp.axis["right"].label.set_color(p3.get_color())
+        # ax.tick_params(axis='x', rotation=45)
+        # ax_prcp.tick_params(axis='x', rotation=45)
+        # ax_temp.tick_params(axis='x', rotation=45)
+
         plt.draw()
         plt.show()
+
         
     
 if __name__ == "__main__":
