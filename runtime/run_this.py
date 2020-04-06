852bfc51fd9d08015f5d6b8dde0975b40d51a0ac
Denizhan Yigitbas <denizhanyigitbas@gmail.com>
2020-04-05 21:30:34 -0600

    delete unneeded code

--- runtime/run_this.py
+++ runtime/run_this.py
@@ -27,14 +27,3 @@ class FireCLI:
 
 if __name__ == '__main__':
     fire.Fire(FireCLI)
-
-# import fire
-#
-# class Calculator(object):
-#   """A simple calculator class."""
-#
-#   def double(self, number):
-#     return 2 * number
-#
-# if __name__ == '__main__':
-#   fire.Fire(Calculator)
